3e7a23dd3661b12257c585e22315af5108adca34
Vivian Hsu <Vivian@Vivians-MacBook-Pro.local>
2018-01-07 16:21:21 -0500

    Implemented insertCounter method in Grid.java and added a Test.java file.

--- src/Grid.java
+++ src/Grid.java
@@ -11,18 +11,26 @@ public class Grid {
   // FUNCTIONS TO BE USED IN Main.java
   /**
    * Return y coordinate of the position of the counter inserted.
-   * @param player The number of the player; either 1 or 2
-   * @param columnNum The number of the column inserted from 1-7.
-   * @return An int from 1-6
+   * @param player The number of the player; either 1 or 2.
+   * @param columnNum The number of the column inserted into from 1-7.
+   * @return An int from 1-6 representing the row number.
    */
   public int insertCounter(int player, int columnNum) {
-    return 0;
+    int rowNum;
+    if(player == 1) {
+      rowNum = addToList(p1Counters, columnNum);
+      grid[columnNum-1][rowNum-1] = 1;
+    } else {
+      rowNum = addToList(p2Counters, columnNum);
+      grid[columnNum-1][rowNum-1] = 2;
+    }
+    return rowNum;
   }
   
   /**
    * Checks if the player won. Returns the positions for the counters which make up the win.
-   * @param player The number of the player; either 1 or 2
-   * @return An array of 4 coordinates
+   * @param player The number of the player; either 1 or 2.
+   * @return An array of 4 coordinates.
    */
   public int[][] checkForWin(int player) {
     return null;
@@ -31,17 +39,43 @@ public class Grid {
   //HELPER FUNCTIONS
   /**
    * Adds the inserted counter to the list of counter positions for the indicated player such
-   * that it is sorted numerically based on x coordinate then y coordinate values.
-   * @param player The number of the player; either 1 or 2
+   * that it is sorted numerically based on the column number then y row number. Returns
+   * the row number of the counter added.
+   * @param pCounters A list of the player's counter positions.
+   * @param int columnNum The number of the column inserted into from 1-7.
+   * @return An int from 1-6 representing the row number.
    */
-  private void addToList(int player, int columnNum) {
+  private int addToList(ArrayList<int[]> pCounters, int columnNum) {
+    // Find the first empty row in that column.
+    int[] rows = grid[columnNum-1];
+    int rowNum = 0;
+    while (rows[rowNum] != 0) {
+      rowNum++;
+    }
+    // Add the counter to pCounters.
+    int[] addedPosition = new int[2];
+    addedPosition[0] = columnNum;
+    addedPosition[1] = rowNum+1;
     
+    int size = pCounters.size();
+    int i = 0;
+    while (i != size && columnNum < pCounters.get(i)[0]) {
+      i++;
+    }
+    pCounters.add(i, addedPosition);
+    
+    System.out.println("Items in pCounters:"); // REMOVE from here to
+    for (int[] j: pCounters){
+      System.out.println(" {" + j[0] + "," + j[1] + "}");
+    } // HERE
+    
+    return rowNum+1;
   }
   
   /**
    * Checks vertically for a win for the indicated player.
-   * @param player The number of the player; either 1 or 2
-   * @return An array of 4 coordinates
+   * @param player The number of the player; either 1 or 2.
+   * @return An array of 4 coordinates.
    */
   private int[][] checkVertical(int player) {
     return null;
@@ -49,8 +83,8 @@ public class Grid {
   
   /**
    * Checks horizontally for a win for the indicated player.
-   * @param player The number of the player; either 1 or 2
-   * @return An array of 4 coordinates
+   * @param player The number of the player; either 1 or 2.
+   * @return An array of 4 coordinates.
    */
   private int[][] checkHorizontal(int player) {
     return null;
@@ -58,8 +92,8 @@ public class Grid {
   
   /**
    * Checks diagonally for a win for the indicated player.
-   * @param player The number of the player; either 1 or 2
-   * @return An array of 4 coordinates
+   * @param player The number of the player; either 1 or 2.
+   * @return An array of 4 coordinates.
    */
   private int[][] checkDiagonal(int player) {
     return null;
---
+++ src/Test.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Test {
+  
+  public static void main(String args[]){
+    Grid grid = new Grid();
+    int rowNum = grid.insertCounter(1, 1);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 1);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 2);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 1);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 1);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 2);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 1);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+    
+    rowNum = grid.insertCounter(1, 2);
+    System.out.println("Item added at row: " + rowNum);
+    System.out.println("");
+
+  }
+}
